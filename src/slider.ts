966ec3be3762dec4e92bcdcfef1965fc0ba7833e
Wouter Hisschemöller <wouter@electricbrains.nl>
2023-03-31 20:04:55 +0200

    Lazy loading images in the slides.

--- src/slider.ts
+++ src/slider.ts
@@ -1,34 +1,30 @@
-import Splide, { Components, SlideComponent } from '@splidejs/splide';
+import Splide from '@splidejs/splide';
 import { Drawing, getDrawings, getVisibleIds, getVisibleIndices } from './data';
 
 const splideEl = document.querySelector('.splide') as HTMLElement;
 const splideList = splideEl.querySelector('.splide__list') as HTMLElement;
 let splide: Splide;
 
-export function setupSlider() {
-  const drawings = getDrawings();
-  const htmlString = drawings.reduce((accumulator, drawing) => (accumulator + `<div class="splide__slide">
-    Slide ${drawing.index} ${drawing.description} (${drawing.year})
-  </div>`), '');
-  splideList.innerHTML = htmlString;
-  splide = new Splide(splideEl).mount();
+function getSlideHtml(drawing: Drawing) {
+  return `<div class="splide__slide">
+      <img data-splide-lazy="images/drawings/${drawing.image_file_large}" alt="${drawing.description}">
+      <div>Slide ${drawing.index} ${drawing.description} (${drawing.year})</div>
+    </div>`;
 }
 
-// export function filterSlides() {
-//   const indices = getVisibleIndices();
-//   splide.Components.Slides.filter((slide: SlideComponent) => !indices.includes(slide.index));
-//   console.log('indices', indices);
-//   console.log('Slides.getLength', splide.Components.Slides.getLength());
-// }
+export function setupSlider() {
+  const drawings = getDrawings();
+  const htmlString = drawings.reduce((accumulator, drawing) => (accumulator + getSlideHtml(drawing)), '');
+  splideList.innerHTML = htmlString;
+  splide = new Splide(splideEl, { lazyLoad: 'nearby' }).mount();
+}
 
 export function updateSlides() {
   const ids = getVisibleIds();
   const drawings = getDrawings();
   const htmlString = ids.map((id) => {
     const drawing = drawings.find((drawing) => drawing.id === id) as Drawing;
-    return `<div class="splide__slide">
-        Slide ${drawing.index} ${drawing.description} (${drawing.year})
-      </div>`;
+    return getSlideHtml(drawing);
   });
   splide.Components.Slides.remove(() => true);
   splide.Components.Slides.add(htmlString);
